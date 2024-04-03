735320691e3c0a9b05e8b9e22b3eab4629097078
xEncerx <109633237+xEncerx@users.noreply.github.com>
2024-04-04 00:25:01 +0300

    convert indents

--- bot/handlers/client/start.py
+++ bot/handlers/client/start.py
@@ -11,34 +11,34 @@ router = Router()
 # Обработка /start + регистрация клиента
 @router.message(Command("start"))
 async def start(message: types.Message, state: FSMContext):
-	await state.clear()
+    await state.clear()
 
-	is_registered = await db.client.exists(message.from_user.id)
-	if not is_registered:
-		referrer_id = message.text[7:]
-		if referrer_id == "":
-			await db.client.add(message.from_user.id, username=message.from_user.username)
+    is_registered = await db.client.exists(message.from_user.id)
+    if not is_registered:
+        referrer_id = message.text[7:]
+        if referrer_id == "":
+            await db.client.add(message.from_user.id, username=message.from_user.username)
 
-		else:
-			if referrer_id == str(message.from_user.id):
-				await bot.send_message(message.from_user.id, "Это ваша же ссылка 😰")
-				return
+        else:
+            if referrer_id == str(message.from_user.id):
+                await bot.send_message(message.from_user.id, "Это ваша же ссылка 😰")
+                return
 
-			else:
-				try:
-					await db.client.add(message.from_user.id, referrer_id, message.from_user.username)
-					referrer_balance = await db.client.get(referrer_id, "balance")
-					new_balance = referrer_balance + BALANCE_FOR_REFERRAL
-					await db.client.update(referrer_id, balance=new_balance)
-					await bot.send_message(referrer_id,
-										   TEXT.new_referrer.format(
-											   username=message.from_user.username,
-											   balance_for_referral=BALANCE_FOR_REFERRAL,
-											   new_balance=new_balance)
-										   )
-				except: pass
+            else:
+                try:
+                    await db.client.add(message.from_user.id, referrer_id, message.from_user.username)
+                    referrer_balance = await db.client.get(referrer_id, "balance")
+                    new_balance = referrer_balance + BALANCE_FOR_REFERRAL
+                    await db.client.update(referrer_id, balance=new_balance)
+                    await bot.send_message(referrer_id,
+                                           TEXT.new_referrer.format(
+                                               username=message.from_user.username,
+                                               balance_for_referral=BALANCE_FOR_REFERRAL,
+                                               new_balance=new_balance)
+                                           )
+                except: pass
 
-	await text_editor(TEXT.welcome,
-					  event=message,
-					  photo=get_image.welcome,
-					  reply_markup=nav.welcome_menu(message.from_user.id))
+    await text_editor(TEXT.welcome,
+                      event=message,
+                      photo=get_image.welcome,
+                      reply_markup=nav.welcome_menu(message.from_user.id))
