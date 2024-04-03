8f28ff7328ddeba567391a36f625e4c6af813f74
xEncerx <109633237+xEncerx@users.noreply.github.com>
2024-04-04 00:30:34 +0300

    convert indents

--- main.py
+++ main.py
@@ -17,36 +17,36 @@ label = """
 
 # Запуск бота и базовых функций
 async def main():
-	await db.create_db()  # Проверка БД + создание при необходимости
+    await db.create_db()  # Проверка БД + создание при необходимости
 
-	me = await bot.get_me()
-	if me.username != BOT_USERNAME:
-		logger.error("Bot Username doesn't match the one specified in the config!")
+    me = await bot.get_me()
+    if me.username != BOT_USERNAME:
+        logger.error("Bot Username doesn't match the one specified in the config!")
 
-	await set_commands(bot)
-	setup_middleware(dp)
-	dp.include_routers(*routers())
+    await set_commands(bot)
+    setup_middleware(dp)
+    dp.include_routers(*routers())
 
-	await bot.delete_webhook(drop_pending_updates=True)
+    await bot.delete_webhook(drop_pending_updates=True)
 
-	await dp.start_polling(bot)
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-	print(label,
-		  "-_-_- Bot made by @Encer -_-_-",
-		  f"-_-_- Version: {__version__} -_-_-",
-		  "-_-_- Thanks for using -_-_-",
-		  sep="\n")
-	try:
-		# Исправление "RuntimeError: Event loop is closed" для Windows
-		# if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith("win"):
-		#     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    print(label,
+          "-_-_- Bot made by @Encer -_-_-",
+          f"-_-_- Version: {__version__} -_-_-",
+          "-_-_- Thanks for using -_-_-",
+          sep="\n")
+    try:
+        # Исправление "RuntimeError: Event loop is closed" для Windows
+        # if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith("win"):
+        #     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-		asyncio.run(main())
-	except (KeyboardInterrupt, SystemExit):
-		logger.warning("-_-_- Bot was stopped -_-_-")
-	finally:
-		if sys.platform.startswith("win"):
-			os.system("cls")
-		else:
-			os.system("clear")
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.warning("-_-_- Bot was stopped -_-_-")
+    finally:
+        if sys.platform.startswith("win"):
+            os.system("cls")
+        else:
+            os.system("clear")
