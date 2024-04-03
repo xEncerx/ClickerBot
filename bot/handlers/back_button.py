1c7f762cee4a346a8914af0fe8155bc0a821407b
xEncerx <109633237+xEncerx@users.noreply.github.com>
2024-04-04 00:22:21 +0300

    Update back_button.py

--- bot/handlers/back_button.py
+++ bot/handlers/back_button.py
@@ -12,35 +12,35 @@ router = Router()
 # Кнопка назад
 @router.callback_query(StateFilter("*"), StartsWith("back|"))
 async def back_handler(call: types.CallbackQuery, state: FSMContext):
-	action = call.data[5:]
-	if action == "add_promo_menu":
-		await text_editor(text=TEXT.admin.promo.add_menu,
-						  event=call,
-						  reply_markup=nav.create_promo_menu())
-		return
+    action = call.data[5:]
+    if action == "add_promo_menu":
+        await text_editor(text=TEXT.admin.promo.add_menu,
+                          event=call,
+                          reply_markup=nav.create_promo_menu())
+        return
 
-	if action == "add_task_menu":
-		await text_editor(TEXT.admin.task.add_menu,
-						  event=call,
-						  reply_markup=nav.add_task())
-		return
+    if action == "add_task_menu":
+        await text_editor(TEXT.admin.task.add_menu,
+                          event=call,
+                          reply_markup=nav.add_task())
+        return
 
-	if await state.get_state():
-		await state.clear()
+    if await state.get_state():
+        await state.clear()
 
-	if action == "main_menu":
-		await text_editor(TEXT.main_menu,
-						  event=call,
-						  photo=get_image.welcome,
-						  reply_markup=nav.main_menu(call.from_user.id))
+    if action == "main_menu":
+        await text_editor(TEXT.main_menu,
+                          event=call,
+                          photo=get_image.welcome,
+                          reply_markup=nav.main_menu(call.from_user.id))
 
-	if action == "earn_menu":
-		await text_editor(text=TEXT.earn,
-						  event=call,
-						  reply_markup=nav.earn_keyboard())
+    if action == "earn_menu":
+        await text_editor(text=TEXT.earn,
+                          event=call,
+                          reply_markup=nav.earn_keyboard())
 
-	if action == "admin_menu":
-		await text_editor(TEXT.admin.admin_menu.format(name=call.from_user.first_name),
-						  event=call,
-						  photo=get_image.admin_menu,
-						  reply_markup=nav.admin_menu())
+    if action == "admin_menu":
+        await text_editor(TEXT.admin.admin_menu.format(name=call.from_user.first_name),
+                          event=call,
+                          photo=get_image.admin_menu,
+                          reply_markup=nav.admin_menu())
